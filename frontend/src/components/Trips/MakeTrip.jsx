1bcfe19869d59ed6dfe978f483bdbedcf2d3ed6e
ncioffi1 <imnickcioffi@gmail.com>
2024-01-30 14:35:20 -0500

    backend fixes

--- frontend/src/components/Trips/MakeTrip.jsx
+++ frontend/src/components/Trips/MakeTrip.jsx
@@ -6,10 +6,16 @@ import './MakeTrip.css'
 
 function MakeTrip() {
     let [location, setLocation] = useState("USA");
-    let [dates, setDates] = useState(["March 1", "March 2"]);
+    let [dates, setDates] = useState([]);
+    let [newDate, setNewDate] = useState("");
     let sessionUser = useSelector(state => state.session.user);
+    let newTrip = useSelector(state => state.trips.new);
 
     const dispatch = useDispatch();
+
+    useEffect(() => {
+        console.log(newTrip);
+    }, [newTrip])
 
     function handleCreateTrip(e) {
         e.preventDefault();
@@ -17,13 +23,45 @@ function MakeTrip() {
         dispatch(tripActions.composeTrip({location, dates, authorId}));
     }
 
-    return (
-        <div className="holder4">
-            <p>Location: </p>
-            <p>Dates: </p>
-            <button onClick={(e) => handleCreateTrip(e)} className='tripButton'>Create Trip</button>
-        </div>
+    function handleAddDate(e) {
+        e.preventDefault();
 
+        let arr2 = [];
+        for (let i = 0; i < dates.length; i++) {
+            arr2.push(dates[i]);
+        }
+        arr2.push(newDate);
+        setDates(arr2);
+        console.log(dates);
+    }
+    function handleClearDates(e) {
+        e.preventDefault();
+
+        setDates([]);
+        console.log(dates);
+    }
+
+    return (
+        <>
+            <div className="tripHolder">
+            <p className="tripText">Location: </p>
+            <input className="tripInput" onChange={(e) => setLocation(e.target.value)} value={location}></input>
+            <p className="tripText">Dates: </p>
+            {dates.map((date) => 
+                <p className="tripText" key={date.toString()}>{date.toString()}</p>
+            )}
+            <input className="tripInput" onChange={(e) => setNewDate(e.target.value)} value={newDate}></input>
+            <div className="tripHolder2">
+                <button onClick={(e) => handleAddDate(e)} className='tripButton'>Add Date</button>
+                <button onClick={(e) => handleClearDates(e)} className='tripButton'>Clear Dates</button>
+                <button onClick={(e) => handleCreateTrip(e)} className='tripButton'>Create Trip</button>
+            </div>
+            </div>
+            <div className="tripHolder">
+
+
+            </div>
+        </>
     )
 }
 
