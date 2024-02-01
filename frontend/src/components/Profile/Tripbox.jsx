ec486485f654c67ddd4262c149e3e1112f01d1b8
Almoe099 <almoe9@LAPTOP-I8VP6L2K>
2024-02-01 11:42:40 -0500

    added pop up modal on delete to confirm delete

--- frontend/src/components/Profile/Tripbox.jsx
+++ frontend/src/components/Profile/Tripbox.jsx
@@ -1,17 +1,18 @@
-// TripBox.js
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Tripbox.css';
-import Share from '../Share/Share';
+// import Share from '../Share/Share';
 
-function TripBox({ trip, onDelete, onView, onShare }) {
+function TripBox({ trip, onDelete, onView }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
 
-  const shareUrl = `http://localhost:5173/trips/${trip._id}`;
-  const shareText = `Check out my upcoming trip to ${trip.location} from ${trip.startdate} to ${trip.enddate}. Check out the details at`;
+  const handleDelete = () => {
+    onDelete(); // Call the onDelete function only if the user confirms the delete action
+    closeModal(); // Close the modal after deleting
+  };
 
   return (
     <div className="trip-box">
@@ -24,18 +25,36 @@ function TripBox({ trip, onDelete, onView, onShare }) {
             View
           </button>
         </Link>
-        <button onClick={onDelete} className="tripBoxButtons">
+        <button onClick={openModal} className="tripBoxButtons">
           Delete
         </button>
-        {onShare && (
-            <Share
-              url={shareUrl}
-              title="Upcoming Trip Details"
-              text={shareText}
-              onClose={closeModal}
-            />
-        )}
+        {/* {onShare && (
+          <Share
+            url={`http://localhost:5173/trips/${trip._id}`}
+            title="Upcoming Trip Details"
+            text={`Check out my upcoming trip to ${trip.location} from ${trip.startdate} to ${trip.enddate}. Check out the details at`}
+            onClose={closeModal}
+          />
+        )} */}
       </div>
+
+      {/* Delete Confirmation Modal */}
+      {isModalOpen && (
+        <div className="delete-modal-overlay">
+          <div className="delete-modal-content">
+            <h2>Delete Trip</h2>
+            <p>Are you sure you want to delete this trip?</p>
+            <div className='delete-modal-buttons'>
+            <button id="delete-confirm" onClick={handleDelete}>
+              Delete
+            </button>
+            <button id="delete-cancel" onClick={closeModal}>
+              Cancel
+            </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
